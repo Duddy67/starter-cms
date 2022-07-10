17e584ef66aef265b561fe18319c19b372c1259a
Lucas <lucas.sanner@codalia.fr>
2022-07-10 09:02:33 +0000

    Fixes small bugs.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -114,7 +114,8 @@ class DatabaseSeeder extends Seeder
             ['group' => 'app', 'key' => 'timezone', 'value' => 'Europe/Paris'],
             ['group' => 'app', 'key' => 'date_format', 'value' => 'd/m/Y H:i'],
             ['group' => 'pagination', 'key' => 'per_page', 'value' => '5'],
-            ['group' => 'website', 'key' => 'allow_registering', 'value' => 1]
+            ['group' => 'website', 'key' => 'allow_registering', 'value' => 1],
+            ['group' => 'website', 'key' => 'redirect_to_admin', 'value' => 0],
             ['group' => 'website', 'key' => 'theme', 'value' => 'starter']
         ]);
     }
