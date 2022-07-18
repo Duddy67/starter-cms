18704d813637dbd541ffb12f2ddd74b58060a546
Lucas <lucas.sanner@codalia.fr>
2022-07-18 08:05:19 +0000

    Improves the filterPostsByCategories method.

--- app/Support/PostCollection.php
+++ app/Support/PostCollection.php
@@ -8,13 +8,34 @@ use Illuminate\Database\Eloquent\Collection;
 
 class PostCollection extends Collection
 {
-    public function filterPostsByCategories(array $categories)
+    public function filterPostsByCategories(array $categories, array $except = [])
     {
-        return $this->filter(function ($post) use($categories) {
+        return $this->filter(function ($post) use($categories, $except) {
+            $slugs = [];
+
             foreach ($post->categories as $category) {
-                if (in_array($category->slug, $categories)) {
-                    return $post;
-                }
+                $slugs[] = $category->slug;
+            }
+
+            // Make sure the post belongs to any of the given categories (OR).
+            if (!empty(array_intersect($categories, $slugs)) && empty(array_intersect($except, $slugs))) {
+                return $post;
+            }
+        });
+    }
+
+    public function filterPostsByAllCategories(array $categories, array $except = [])
+    {
+        return $this->filter(function ($post) use($categories, $except) {
+            $slugs = [];
+
+            foreach ($post->categories as $category) {
+                $slugs[] = $category->slug;
+            }
+
+            // Make sure the post belongs to all the given categories (AND).
+            if (count(array_intersect($categories, $slugs)) == count($categories) && empty(array_intersect($except, $slugs))) {
+                return $post;
             }
         });
     }
