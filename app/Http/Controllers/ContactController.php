98eaaa0f758cdbda0645d8eb8583072c293f27d6
Lucas <lucas.sanner@codalia.fr>
2022-08-29 15:20:41 +0000

    Adds the missing ContactController.php file.

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Setting;
+use App\Models\Email;
+use App\Http\Requests\Contact\StoreRequest;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+class ContactController extends Controller
+{
+    /**
+     * Store a newly sent message through the contact form. (AJAX)
+     *
+     * @param  \App\Http\Requests\Contact\StoreRequest $request
+     * @return JSON
+     */
+    public function store(StoreRequest $request)
+    {
+        $id = DB::table('contacts')->insertGetId([
+            'name' => $request->input('name'), 
+            'email' => $request->input('email'), 
+            'object' => $request->input('object'), 
+            'message' => $request->input('message'), 
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+            'status' => 'unread'
+        ]);
+
+        $contact = DB::table('contacts')->where('id', $id)->first();
+
+        // Set a recipient attribute to prevent the sendEmail function to use the email
+        // attribute as recipient.
+        $contact->recipient = Setting::getValue('website', 'admin_email');
+
+        if (!empty($contact->recipient)) {
+            Email::sendEmail('new_message', $contact);
+        }
+
+        $request->session()->flash('success', __('messages.message.send_success'));
+        // Set the redirect url.
+        $redirect = ($request->input('_page', null)) ? url('/').'/'.$request->input('_page', null) : url('/');
+
+        return response()->json(['redirect' => $redirect]);
+    }
+}
