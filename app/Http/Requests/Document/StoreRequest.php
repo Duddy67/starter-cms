3eb5f92f379051b1c54620e8ee2669094db88973
Lucas <lucas.sanner@codalia.fr>
2022-10-31 12:52:50 +0000

    Adds the forgotten file StoreRequest.ph

---
+++ app/Http/Requests/Document/StoreRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Document;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'upload' => ['nullable', 'image', 'mimes:jpg,png,jpeg,gif,svg', 'max:2048', 'dimensions:min_width=100,min_height=100,max_width=1000,max_height=1000'],
+        ];
+    }
+}
