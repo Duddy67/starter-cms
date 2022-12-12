0ed8a290077f948efcad005bfaee9357965c0c6b
Lucas <lucas.sanner@codalia.fr>
2022-12-12 12:16:52 +0000

    French translation.

--- lang/fr/passwords.php
+++ lang/fr/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Votre mot de passe a été réinitialisé!',
+    'sent' => 'Un email de réinitialisation de mot de passe vous a été envoyé!',
+    'throttled' => 'Veuillez attendre avant de réessayer.',
+    'token' => 'Ce jeton de réinitialisation de mot de passe est invalide.',
+    'user' => "Aucun utilisateur trouvé possédant cette adresse email.",
 
 ];
