a1e64257cdceefc1e18a70a99c569695eeee1086
Lucas <lucas.sanner@codalia.fr>
2024-01-28 09:12:55 +0000

    Adapts both the login and register layouts on front-end.

--- resources/views/admin/share/batch.blade.php
+++ resources/views/admin/share/batch.blade.php
@@ -7,8 +7,6 @@
 
 	<!-- Google Font: Source Sans Pro -->
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-	<!-- Font Awesome Icons -->
-	<!--<link rel="stylesheet" href="{{ asset('/vendor/adminlte/plugins/fontawesome-free/css/all.min.css') }}">-->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css">
         <!-- Select2 plugin style -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css">
