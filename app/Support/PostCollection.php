6c4eb3b2cd9a059ed1bd17810863d3bc1386e5bf
Lucas <lucas.sanner@codalia.fr>
2025-02-17 13:53:04 +0000

    Adds new function that returns posts of the given category sorted by the given column.

--- app/Support/PostCollection.php
+++ app/Support/PostCollection.php
@@ -8,41 +8,106 @@ use Illuminate\Database\Eloquent\Collection;
 
 class PostCollection extends Collection
 {
-    public function filterPostsByCategories(array $categories, array $except = [])
+    /*
+     * Returns posts from a category sorted by the given column.
+     *
+     * @param string  $slug    The slug of the category to filter from.
+     * @param string  $column  The column by which posts are sorted (Optional).
+     * @param boolean $desc    When set to true, posts are sorted in the opposite order (Optional).
+     *
+     * @return Object  The sorted posts filtered by the given category.
+     */
+    public function filterPostsByCategory(string $slug, string $column = '', bool $desc = false)
     {
-        return $this->filter(function ($post) use($categories, $except) {
-            $slugs = [];
+        if (!empty($column)) {
+            // Sort posts by the given column.
+            $posts = ($desc) ? $this->sortByDesc($column) : $this->sortBy($column);
+        }
+        else {
+            $posts = $this;
+        }
+
+        return $posts->filter(function ($post) use($slug) {
+            // Loop through the post's categories.
+            foreach ($post->categories as $category) {
+                // Check for the given slug.
+                if ($category->slug == $slug) {
+                    return $post;
+                }
+            }
+        });
+    }
+
+    /*
+     * Returns posts filtered by any of the given categories (OR clause).
+     *
+     * @param array $slugs    The slug of the categories to filter from.
+     * @param array $except   The slug of the categories to ignore (Optional).
+     *
+     * @return Object  The posts filtered by the given categories.
+     */
+    public function filterPostsByCategories(array $slugs, array $except = [])
+    {
+        return $this->filter(function ($post) use($slugs, $except) {
+            $results = [];
 
             foreach ($post->categories as $category) {
-                $slugs[] = $category->slug;
+                // Store the slug of the categories the post belongs to.
+                $results[] = $category->slug;
             }
 
             // Make sure the post belongs to any of the given categories (OR).
-            if (!empty(array_intersect($categories, $slugs)) && empty(array_intersect($except, $slugs))) {
+            if (!empty(array_intersect($slugs, $results)) && empty(array_intersect($except, $results))) {
                 return $post;
             }
         });
     }
 
-    public function filterPostsByAllCategories(array $categories, array $except = [])
+    /*
+     * Returns posts filtered by all of the given categories (AND clause).
+     *
+     * @param array $slugs    The slug of the categories to filter from.
+     * @param array $except   The slug of the categories to ignore (Optional).
+     *
+     * @return Object  The posts filtered by the given categories.
+     */
+    public function filterPostsByAllCategories(array $slugs, array $except = [])
     {
-        return $this->filter(function ($post) use($categories, $except) {
-            $slugs = [];
+        return $this->filter(function ($post) use($slugs, $except) {
+            $results = [];
 
             foreach ($post->categories as $category) {
-                $slugs[] = $category->slug;
+                // Store the slug of the categories the post belongs to.
+                $results[] = $category->slug;
             }
 
             // Make sure the post belongs to all the given categories (AND).
-            if (count(array_intersect($categories, $slugs)) == count($categories) && empty(array_intersect($except, $slugs))) {
+            if (count(array_intersect($slugs, $results)) == count($slugs) && empty(array_intersect($except, $results))) {
                 return $post;
             }
         });
     }
 
-    public function filterPostsBySlug(array $slugs)
+    /*
+     * Returns posts filtered by slug and sorted by the given column.
+     *
+     * @param array   $slugs   The slug of the posts to filter.
+     * @param string  $column  The column by which posts are sorted (Optional).
+     * @param boolean $desc    When set to true, posts are sorted in the opposite order (Optional).
+     *
+     * @return Object  The filtered posts.
+     */
+    public function filterPostsBySlug(array $slugs, string $column = '', bool $desc = false)
     {
-        return $this->filter(function ($post) use($slugs) {
+        if (!empty($column)) {
+            // Sort posts by the given column.
+            $posts = ($desc) ? $this->sortByDesc($column) : $this->sortBy($column);
+        }
+        else {
+            $posts = $this;
+        }
+
+        return $posts->filter(function ($post) use($slugs) {
             if (in_array($post->slug, $slugs)) {
                 return $post;
             }
--- resources/views/themes/starter/pages/home.blade.php
+++ resources/views/themes/starter/pages/home.blade.php
@@ -1,14 +1,34 @@
 
 @if (isset($category) && $category)
-    <ul class="post-list">
-	@if (isset($posts) && count($posts))
+    @if (isset($posts) && count($posts))
+        @php $selections = $posts->filterPostsBySlug(['lolita', '1984'], 'created_at', true); @endphp 
+        @if (!$selections->isEmpty())
+            <h2 class="mt-5">Our Selection</h2>
+            <ul class="list-group list-group-horizontal">
+                @foreach ($selections as $post)
+                    @include ('themes.starter.partials.post')
+                @endforeach
+            </ul>
+        @endif
+
+        <ul class="post-list mt-5">
 	    @foreach ($posts as $post)
 		@include ('themes.starter.partials.post')
 	    @endforeach
-	@else
-	    <div>No post</div>
-	@endif
-    </ul>
+        </ul>
+
+        @php $discounts = $posts->filterPostsByCategory('discount', 'created_at'); @endphp 
+        @if (!$discounts->isEmpty())
+            <h2 class="mt-5">Discounts</h2>
+            <ul class="list-group list-group-horizontal">
+                @foreach ($discounts as $post)
+                    @include ('themes.starter.partials.post')
+                @endforeach
+            </ul>
+        @endif
+    @else
+        <div>No post</div>
+    @endif
 
     @if ($category->getExtraFieldByAlias('library'))
         <span>Library: </span>{{ $category->getExtraFieldByAlias('library') }}
